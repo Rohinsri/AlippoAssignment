f056f51400a51c5397197fa5100a58c44dc66b85
Rohinsri <61387189+Rohinsri@users.noreply.github.com>
2023-08-10 01:38:42 +0530

    Added Pagination Logic to Products Screen

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -13,12 +13,37 @@ class ProductListPage extends StatefulWidget {
 }
 
 class _ProductListPageState extends State<ProductListPage> {
-  Future<List<Product>> fetchProducts() async {
-    final response = await http.get(Uri.parse('https://dummyjson.com/products'));
-    if (response.statusCode == 200) {
-      final List<dynamic> jsonData = json.decode(response.body)['products'];
-      return jsonData.map((productJson) => Product.fromJSON(productJson)).toList();
 
+  int _skip = 0;
+  final int _limit = 10;
+  bool _isLoading = false;
+  final ScrollController _scrollController = ScrollController();
+  late Future<List<Product>> _productsFuture;
+  final List<Product> _products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_productsScrollListener);
+    _productsFuture = fetchProducts(_skip,_limit);
+  }
+
+  void _productsScrollListener() {
+    if (!_isLoading && _scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+      setState(() {
+        _isLoading = true;
+        _skip=_skip+_limit;
+        _productsFuture = fetchProducts(_skip, _limit);
+      });
+    }
+  }
+
+  Future<List<Product>> fetchProducts(int skip, int limit) async {
+    final response = await http.get(Uri.parse('https://dummyjson.com/products?skip=$skip&page=$limit'));
+    if (response.statusCode == 200) {
+      final productsData = json.decode(response.body)['products'] as List<dynamic>;
+      final List<Product> products = productsData.map((productJson) => Product.fromJSON(productJson)).toList();
+      return products;
     } else {
       throw Exception('Failed to load products');
     }
@@ -28,7 +53,7 @@ class _ProductListPageState extends State<ProductListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder<List<Product>>(
-        future: fetchProducts(),
+        future: _productsFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
@@ -36,10 +61,16 @@ class _ProductListPageState extends State<ProductListPage> {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else {
             final products = snapshot.data!;
+            _products.addAll(products);
+            _isLoading = false;
             return ListView.builder(
-              itemCount: products.length,
+              controller: _scrollController,
+              itemCount: _products.length,
               itemBuilder: (context, index) {
-                final product = products[index];
+                if(index==_products.length){
+                  return const Center(child: CircularProgressIndicator());
+                }
+                final product = _products[index];
                 return ProductListItem(product: product);
               },
             );
