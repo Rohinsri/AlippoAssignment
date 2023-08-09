79f96f45dc9324ffd2db98d0fe233d69e95480fe
Rohinsri <sri.rohin184@gmail.com>
2023-08-10 01:29:03 +0530

    Adding Pagination Logic to Carts and Products Screen

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -13,12 +13,35 @@ class CartPage extends StatefulWidget {
 
 class _CartPageState extends State<CartPage> {
 
-  Future<List<Cart>> fetchCart() async {
-    final response = await http.get(Uri.parse('https://dummyjson.com/carts'));
+  int _skip = 0;
+  final int _limit = 10;
+  bool _isLoading = false;
+  final ScrollController _scrollController = ScrollController();
+  late Future<List<Cart>> _cartsFuture;
+  final List<Cart> _carts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_cartScrollListener);
+    _cartsFuture = fetchCarts(_skip,_limit);
+  }
+
+  void _cartScrollListener() {
+    if (!_isLoading && _scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+      setState(() {
+        _isLoading = true;
+        _skip=_skip+_limit;
+        _cartsFuture = fetchCarts(_skip, _limit);
+      });
+    }
+  }
+
+  Future<List<Cart>> fetchCarts(int skip, int limit) async {
+    final response = await http.get(Uri.parse('https://dummyjson.com/carts?skip=$skip&page=$limit'));
     if (response.statusCode == 200) {
       final List<dynamic> jsonData = json.decode(response.body)['carts'];
       return jsonData.map((cartJson) => Cart.fromJSON(cartJson)).toList();
-
     } else {
       throw Exception('Failed to load carts');
     }
@@ -28,7 +51,7 @@ class _CartPageState extends State<CartPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder<List<Cart>>(
-        future: fetchCart(),
+        future: _cartsFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
@@ -36,10 +59,15 @@ class _CartPageState extends State<CartPage> {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else {
             final carts = snapshot.data!;
+            carts.addAll(_carts);
+            _isLoading = false;
             return ListView.builder(
-              shrinkWrap: true,
+              controller: _scrollController,
               itemCount: carts.length,
               itemBuilder: (context, index) {
+                if(index==carts.length){
+                  return const Center(child: CircularProgressIndicator());
+                }
                 final cart = carts[index];
                 return CartListItem(cart: cart);
               },
